e8fa6bf4170bbc1de6dcfa01348147b86bf19f39
Michael Karrer <michaelkarrer81@gmail.com>
2017-08-03 00:29:53 +0200

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/kich_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'kich_config',
+    'summary': '''FS-Online kich instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance kich
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
